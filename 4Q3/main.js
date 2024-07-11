37816a3cb6c40abccb1db6f34794ff8537874ed2
khg5900 <37091492+khg5900@users.noreply.github.com>
2024-07-11 22:08:49 +0900

    main.js

--- 4Q3/main.js
+++ 4Q3/main.js
@@ -4,7 +4,7 @@ numArr[0] = ++numArr[0];
 numArr[1] = numArr[1] * 2;
 console.log(numArr);
 console.log(numArr.length);
-numArr[numArr.length - 1] = 100;
+numArr[numArr.length - 1] = 100; // 마지막에 생성을 다른방법으로 표현하기
 console.log(numArr.length);
 numArr[numArr.length - 1] = numArr[numArr.length - 1] * 2;
 console.log(numArr.length);
